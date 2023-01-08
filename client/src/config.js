3bbaa0cd12ea55de0b8dd945747415d544047960
david <austinedavid96@gmail.com>
2023-01-08 15:40:10 +0100

    changed my backend urls direction

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "https://laptopdashboard.herokuapp.com/api/"
+    baseURL: "https://laptopbackend.onrender.com/api"
 })
